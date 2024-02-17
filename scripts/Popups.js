4f2a209fe785691fae1d037d8f52cab4c5ca668e
HanaXHub <hana.2601@outlook.com>
2024-02-18 03:55:31 +0400

    update to Popup to add alert

--- scripts/Popups.js
+++ scripts/Popups.js
@@ -25,7 +25,17 @@ function generateHTML(headerMessage, message) {
 function confirmation(headerMessage, message) {
     var popup = document.createElement('div');
     popup.className = 'popup-container';
-    popup.innerHTML = generateHTML(headerMessage, message); // Generate HTML with provided headerMessage and message
+    popup.innerHTML = generateHTML(headerMessage, message); 
+    document.body.appendChild(popup);
+}
+
+function alert(headerMessage, message) {
+    var popup = document.createElement('div');
+    popup.className = 'popup-container';
+    var html = generateHTML(headerMessage, message);
+    // Remove the second button by replacing its entire tag with an empty string
+    html = html.replace('<button class="button1" onClick="hidePopup()">No</button>', '').replace('<button class="button2" onClick="hidePopup()">Yes</button>', '<button class="button2" onClick="hidePopup()">Ok</button>');
+    popup.innerHTML = html;
     document.body.appendChild(popup);
 }
 
