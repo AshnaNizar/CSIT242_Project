857d05e0b7394cbc93ab8227273dc601469c1e76
Ashna Nizar <ashunizar@gmail.com>
2024-02-18 15:23:31 +0400

    Home pg added, login css fixes

--- scripts/Login.js
+++ scripts/Login.js
@@ -1,17 +1,16 @@
 function validatePassword() {
-    //check if passwords are equal
+    // Check if passwords are equal
     var pass = document.getElementById("password").value;
-    var lowercaseCheck= /[a-z]/;
+    var lowercaseCheck = /[a-z]/;
     var upperCaseCheck = /[A-Z]/;
-    var specialNumCheck=/[^a-zA-Z\d\s:]/;
-    if(!lowercaseCheck.test(pass.trim())|!upperCaseCheck.test(pass.trim())|!specialNumCheck.test(pass.trim())|pass.trim().length<6 | pass.trim().length>15){
+    var specialNumCheck = /[^a-zA-Z\d\s:]/;
+    var numCheck = /[0-9]/;
+
+    if (!lowercaseCheck.test(pass.trim()) | !upperCaseCheck.test(pass.trim()) | !specialNumCheck.test(pass.trim()) | !numCheck.test(pass.trim()) | pass.trim().length < 6 | pass.trim().length > 15) {
         document.getElementById("passError").innerHTML = "Incorrect Password";
         return false;
-
     }
-
 
     window.location.href = '../pages/Signup.html';
     return false;
-
 }
